ea107f73ffa0ebbca2a5adf32f346f2d944b0436
gayatriijadeja <121252280+gayatriijadeja@users.noreply.github.com>
2024-03-12 22:01:52 +0530

    Update route.py

--- routes/route.py
+++ routes/route.py
@@ -15,3 +15,18 @@ router = APIRouter()
 async def get_iocs():
     iocs = list_serial(collection_name.find())
     return iocs     
+
+#Post request method
+@router.post("/")
+async def post_iocs(ioc: str):
+    collection_name.insert_one(dict(ioc))
+
+#Put request method
+@router.put("/{id}")
+async def put_iocs( id : str, ioc : str):
+    collection_name.find_one_and_update({"_id" : ObjectId (id)}, {"$set": dict(ioc)})
+
+#Delete request method
+@router.delete("\{id}")
+async def delete_iocs(id: str):
+    collection_name.find_one_and_delete({"_id" : ObjectId(id)})
